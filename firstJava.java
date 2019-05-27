4faee8ba2455feb65d31ebbdf876134c36ca6ce3
Danny <yanez.daniel0101@gmail.com>
2019-05-27 17:02:31 -0500

    inital commmit

---
+++ firstJava.java
@@ -0,0 +1,7 @@
+public class firstJava {
+	public static void main (String args[]){
+		System.out.println("Hello my name is Daniel");
+		System.out.println("I am 30 years old");
+		System.out.println("I am from Dallas, Texas");
+	}
+}
